05bb1fa812cbdf8e951dfa10901f68ebcadb786a
jefflunt <jefflunt@gmail.com>
2021-12-06 08:20:11 -0600

    Day 1, parts 1 and 2

---
+++ day2p2.rb
@@ -0,0 +1,31 @@
+# Parses a command like:
+#   foward 2
+#   down 5
+#   up 3
+#
+# ... and returns a 2-element array of [horiz_delta, aim_delta]
+def parse_cmd(cmd)
+  cmd
+    .strip
+    .split
+    .each_cons(2)
+    .map do |dir, amt|
+      case dir
+      when 'forward'  then [amt.to_i, 0]
+      when 'up'       then [0, -amt.to_i]
+      when 'down'     then [0, amt.to_i]
+      else
+        raise "unknown cmd: `#{cmd}', split into (#{dir}, #{amt})"
+      end
+    end.flatten
+end
+
+aim = 0
+horiz = 0
+depth = 0
+
+$stdin
+  .read
+  .lines
+  .map{|l| parse_cmd(l) }
+  .each.with_index{|(h, a), i| horiz += h; aim += a; depth += (h * aim); puts "#{i.to_s.rjust(5)}: h: #{horiz}, d: #{depth}, a: #{aim}" }
